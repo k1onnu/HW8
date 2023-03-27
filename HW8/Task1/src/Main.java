fddf2e0bd5698a53cab1fb97c38f639afdd391bf
k1 <tichdies@gmail.com>
2023-03-27 11:01:04 +0300

    complete task1(100%) and task2(90%).

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -3,13 +3,36 @@ public class Main {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
 
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
+
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("");
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
 
+        for(int i = array.length - 1; i >= 0;i--) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.print("");
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = tmp;
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+
+        double[] people = new double[10];
+        for (int i = 0; i < people.length; i++) {
+            people[i] = (int) (40 + Math.random() * 60);
+            System.out.println("Вес " + "человека: " + people[i]);
+        }
+
+        // подсчёт среднего веса всех людей
+        double middleCount = 0;
+        for (int i = 0; i < people.length; i++) {
+            middleCount += people[i];
+        }
+        System.out.println("\nСредний вес всех людей: " + middleCount / people.length);
+
+
+        // подсчёт кол-во людей
+        int peopleNumber = 0;
+        for (int i = 0; i < people.length; i++) {
+            if (people[i] >= 60 && people[i] <= 80) {
+                peopleNumber++;
+            }
+        }
+        System.out.println("Кол-во людей: " + peopleNumber);
     }
 }
